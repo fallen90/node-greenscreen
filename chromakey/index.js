47de0ae9876ef518e4c862ce958e1410d39189c6
fallen90 <slytherin9090@gmail.com>
2016-03-23 20:30:59 +0800

    fix chromakey

--- chromakey/index.js
+++ chromakey/index.js
@@ -3,22 +3,31 @@ var fs = require('fs'),
     easyimg = require('easyimage'),
     path = require('path');
 
+
+var exec = require('child_process').exec;
+
+var orig, src, butas, inter, inter2, output, command1, command1_2, command2, command3, command4, command5;
+
+
+
 exports.create = function(inf, outf, success, failed) {
     if (typeof outf == 'undefined') {
         outf = 'output/' + path.basename(inf, path.extname(inf)) + '.out.png';
     }
-        easyimg.convert({
-            src: inf,
-            dst: ".tmp/" + path.basename(inf, path.extname(inf)) + '.png'
-        }).then(function(file) {
-            processFile(file.path, outf, success, failed);
-        }, function(err) {
-            console.error('error converting image', err, inf, outf);
-            failed(err, inf, outf);
-        });
+    easyimg.convert({
+        src: inf,
+        dst: ".tmp/" + path.basename(inf, path.extname(inf)) + '.png'
+    }).then(function(file) {
+        processFile(file.path, outf, success, failed);
+    }, function(err) {
+        console.error('error converting image', err, inf, outf);
+        failed(err, inf, outf);
+    });
 };
 
 var processFile = function(inf, outf, success, failed) {
+    var dst = fs.createWriteStream(outf);
+
     fs.createReadStream(inf)
         .pipe(new PNG({
             filterType: 4
@@ -43,10 +52,32 @@ var processFile = function(inf, outf, success, failed) {
                     this.data[idx + 3] = newPixel.a;
                 }
             }
-            this.pack().pipe(fs.createWriteStream(outf));
+            this.pack()
+                .pipe(dst);
+
+            dst.on('close', function() {
+                console.log('on end', __dirname);
+                // orig = path.basename(inf, path.extname(inf));
+                // src = orig + ".png";
+                // butas = orig + "_butas.png";
+                // inter = orig + "_inter.png";
+                // inter2 = orig + "_inter2.png";
+                // output = orig + "_output.png";
+
+                // command1 = "composite -geometry 300x250+350+510 " + butas + " ../imgs/bg.png " + inter;
+                // command1_2 = "composite ../imgs/warm1.png " + inter + " " + inter2;
+
+                // command2 = "composite ../imgs/fg.png " + inter2 + " " + output;
+                // command3 = "rm " + butas ;
+                // command4 = "rm " + inter;
+                // command5 = "rm " + inter2;
+
+                // exec(command1, command12func);
+            });
             success(outf);
-        }, function(err){
-            console.log('failed to parsed stream', err, "outf",outf,"inf", inf);
+
+        }, function(err) {
+            console.log('failed to parsed stream', err, "outf", outf, "inf", inf);
             failed(err);
         });
 };
@@ -82,3 +113,41 @@ var operate = function(pixel, alphaMap) {
 
     return pixel;
 };
+
+
+
+function command2func(error, stdout, stderr) {
+    console.log('COMMAND ', command2, '  ERROR:', error);
+    exec(command2, command3func);
+}
+
+function command12func(error, stdout, stderr) {
+    console.log('COMMAND ', command1_2, '  ERROR:', error);
+    exec(command1_2, command2func);
+}
+
+function command3func(error, stdout, stderr) {
+    console.log('COMMAND ', command3, '  ERROR:', error);
+    exec(command3, puts);
+    exec(command4, puts);
+    exec(command5, puts);
+
+}
+
+
+// chromakey.create(src, butas, function(file){
+
+//   console.log( 'FILE', file );
+//   console.log( 'COMMAND 1', command1 );
+
+
+
+// }, function(err){
+//     console.log('failed');
+// });
+
+
+function puts(error, stdout, stderr) {
+    if (error) console.log(error);
+    //sys.puts(stdout) 
+}
