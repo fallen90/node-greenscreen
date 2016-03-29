c67ac2ec47dc9f8f737e3326cf07587bda7d3668
fallen90 <slytherin9090@gmail.com>
2016-03-29 08:49:09 +0800

    changed to match heroku

--- app.js
+++ app.js
@@ -12,10 +12,20 @@ var gdrive = require('./gdrive');
 var upload_dir = __dirname + '/public/uploads/';
 var output_dir = __dirname + '/public/outputs/';
 
+app.set('port', (process.env.PORT || 5000));
+
+app.set('view engine', 'jade');
+app.set('views', 'public/views')
 app.use(busboy());
 
-app.use(express.static('public'));
-
+// app.use(express.static(__dirname + '/public'));
+app.use('/lib', express.static(__dirname + '/public/lib'));
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey'});
+});
+app.get('/download', function (req, res) {
+  res.render('download', { title: 'Hey'});
+});
 app.post('/file-upload', function(req, res) {
     var fstream;
     req.pipe(req.busboy);
@@ -44,13 +54,14 @@ app.post('/file-upload', function(req, res) {
         });
     });
 });
-app.listen(3000, function() {
-    console.log('Server is listening on 3000!');
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 var uploadFile = function(key) {
     console.log('Uploading...');
-    gdrive.uploadFile(key + '.out.png', key + '.out.png', function(result) {
+    gdrive.uploadFile(key + '.out.png', key + '.out.png','0B3zHm_k_51jLWlBUZzVmclJDemc', function(result) {
         console.log('success!!!', result);
     }, function(err) {
         console.log('Error!!!', err);
